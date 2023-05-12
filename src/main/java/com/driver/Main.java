bc6d372930204263f49db089de9d2f0e8c468352
Ruhani FashionFantasy <ruhanifashionfantasy@Ruhanis-MacBook-Air.local>
2023-05-12 18:59:19 +0530

    changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj1=new RWOnly();
+        obj1.setName("Ruhani");
+        String ans=obj1.getName();
+        System.out.println(ans);
+
+    }
 }
